0f5cf93bea79538d5a5f953051a041b0fb97186a
imranmalakzai <afghanimran92@gmail.com>
2025-08-21 21:07:39 +0430

    create 10.php Now we can create Array in php - stander why $varible_name = array("Element1","Element2",...); - shorthand formate is $varible_name = ["Imran","Khan","Wasi"]

---
+++ practical/10.php
@@ -0,0 +1,18 @@
+<?php
+
+//This is the access Indexing
+$number_Arrays = array(1, 2, 3, 34, 34, 34, 34, 34, 34, 3);
+echo ($number_Arrays[0]);
+
+//To change value of an Index
+$number_Arrays[0] = 199;
+
+//This function is used to push number in The Array
+array_push($number_Arrays, 1000);
+
+//create array 
+$cars = array("HONDA", "SOZAKI", "MINBAR", "SAID");
+
+
+//Short Hand creating of the Array
+$motors = ["HONDA", "Lisa", "Mosa", "kingo", "Heloka"];
