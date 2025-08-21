6f524c274d66904921fd7839a53d028c4e947aa1
imranmalakzai <afghanimran92@gmail.com>
2025-08-21 12:23:21 +0430

    Create 01.php Now We can find the max and min number of Array - The max number can be find using max($array) - The min number can be find using min($Array)

---
+++ practical/02.php
@@ -0,0 +1,9 @@
+<?php 
+
+//Find max and min number in php
+
+//Array of numbers
+$numbers = array(1,2343,523,532,432,43,24,23,432,4,3245324,32,432,44234);
+echo(min($numbers));
+// echo(max(0, 150, 30, 20, -8, -200));
+?>
