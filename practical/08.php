7e243189795371aa18cca19ccc8e6928eb1175b0
imranmalakzai <afghanimran92@gmail.com>
2025-08-21 15:33:49 +0430

    create 08.php In php we have Associative array - It works similler like object in javascript - its look like arra("name" => "imran","fathername" => "Abdual wadood")

---
+++ practical/08.php
@@ -0,0 +1,8 @@
+<?php
+
+//Assostive Array in php simmiler like object
+$members = array("Peter" => "35", "Ben" => "37", "Joe" => "43");
+
+foreach ($members as $x => $y) {
+  echo "$x : $y <br>";
+}
