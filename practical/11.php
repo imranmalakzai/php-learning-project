bd9cf1c3be333b75b49e68fe84babacc774f9a7d
imranmalakzai <afghanimran92@gmail.com>
2025-08-22 08:08:34 +0430

    create 11.php Now We can Access Form Data using php - php get method and post method are the to main method for the Form - we can transfor the data to another form by action attrabute in form

---
+++ practical/11.php
@@ -0,0 +1,12 @@
+<html>
+
+<body>
+  <form action="extra/welcome.php" method="POST">
+    Name: <input type="text" name="name"><br>
+    E-mail: <input type="text" name="email"><br>
+    <input type="submit">
+  </form>
+
+</body>
+
+</html>
