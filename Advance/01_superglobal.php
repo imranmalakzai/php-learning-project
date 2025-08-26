54fb0e24231d7d97c6eb54406cccf26abb78e310
imranmalakzai <afghanimran92@gmail.com>
2025-08-26 10:51:07 +0430

    create server and with with super global methods

---
+++ Advance/01_superglobal.php
@@ -0,0 +1,9 @@
+<?php
+
+
+// server superglobal
+
+// var_dump($_SERVER["DOCUMENT_ROOT"]);
+// var_dump($_SERVER["SERVER_NAME"]);
+// var_dump($_SERVER["SERVER_PORT"]);
+// var_dump($_SERVER["SERVER_PROTOCOL"]);
